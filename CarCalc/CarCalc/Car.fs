49e92a8f1ff2aafc4d2ef0bc31215e4c6ab81b78
W. Tony Dean <william.dean@acuitybrands.com>
2021-11-12 13:17:53 -0500

    initial commit

---
+++ CarCalc/CarCalc/Car.fs
@@ -0,0 +1,27 @@
+﻿module Car
+
+open System
+
+//TODO: Create helper functions to provide the building blocks to implement driveTo.
+
+/// Drives to a given destination given a starting amount of petrol
+let getPetrol dest = 
+   if dest = "Home" then 25
+   elif dest = "Office" then 50
+   elif dest = "Stadium" then 25
+   elif dest = "Gas" then 10
+   else failwith "Invalid destination"
+    
+let calcRemainingPetrol currentPetrol usedPetrol =
+    let remainingPetrol = currentPetrol - usedPetrol
+    if remainingPetrol >= 0 then remainingPetrol
+    else failwith "Oops you ran out of petrol"
+
+
+let driveTo currentPetrol destination = 
+    let petrolNeeded = getPetrol destination
+    let remainingPetrol = calcRemainingPetrol currentPetrol petrolNeeded
+    if destination = "Gas" then remainingPetrol + 50
+    else remainingPetrol
+
+
---
+++ ScratchPad/ScratchPad/Program.fs
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.IO
+let writeToFile (date:DateTime) fileName text =
+    let path = "%s-%s.txt" (date.ToString "yyMMdd") fileName
+    File.WriteAllText path text
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
+    
